cfe8137545012e9a5e5a8c0b298871fd35cc6007
Frédéric Devernay <frederic.devernay@m4x.org>
2014-04-05 23:21:39 +0200

    Transform: fix PluginRegistration

--- Transform/PluginRegistration.cpp
+++ Transform/PluginRegistration.cpp
@@ -1,4 +1,4 @@
-#include "Grade.h"
+#include "Transform.h"
 
 namespace OFX
 {
@@ -6,7 +6,7 @@ namespace OFX
     {
         void getPluginIDs(OFX::PluginFactoryArray &ids)
         {
-            static GradePluginFactory p("fr.INRIA.openfx:TransformPlugin", 1, 0);
+            static TransformPluginFactory p("fr.INRIA.openfx:TransformPlugin", 1, 0);
             ids.push_back(&p);
         }
     }
