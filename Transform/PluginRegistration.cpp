0ea9379862ec5e10e3155f2d3981c3bef6aa76cc
Frédéric Devernay <frederic.devernay@m4x.org>
2014-05-28 16:55:45 +0200

    fix Makefiles

--- Transform/PluginRegistration.cpp
+++ Transform/PluginRegistration.cpp
@@ -6,10 +6,10 @@ namespace OFX
     {
         void getPluginIDs(OFX::PluginFactoryArray &ids)
         {
-            static TransformPluginFactory p("net.sf.openfx:TransformPlugin", 1, 0);
-            ids.push_back(&p);
-            static TransformMaskedPluginFactory p("net.sf.openfx:TransformMaskedPlugin", 1, 0);
-            ids.push_back(&p);
+            static TransformPluginFactory p1("net.sf.openfx:TransformPlugin", 1, 0);
+            ids.push_back(&p1);
+            static TransformMaskedPluginFactory p2("net.sf.openfx:TransformMaskedPlugin", 1, 0);
+            ids.push_back(&p2);
         }
     }
 }
