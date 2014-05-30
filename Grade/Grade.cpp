883b8faeea194305ec88d806225eb638f6fb0121
MrKepzie <alexandre230@msn.com>
2014-05-30 13:15:49 +0200

    Grade: add params to a page

--- Grade/Grade.cpp
+++ Grade/Grade.cpp
@@ -464,6 +464,7 @@ void defineRGBAScaleParam(OFX::ImageEffectDescriptor &desc,
     param->setDefault(def,def,def,def);
     param->setRange(min,min,min,min, max,max,max,max);
     param->setDisplayRange(min,min,min,min,max,max,max,max);
+    page->addChild(*param);
 }
 
 
