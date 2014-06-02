NatronGitHub/openfx-misc

commit 0826664938d0af9f211bec7a000359f6094f6e8a

Bug fix: Transform3x3:IsIdentity the version called from getRegionOfDefinition was not the good one hence there was an issue when mix = 0

CornerPin/CornerPin.cpp
@@ -412,16 +412,11 @@ bool CornerPinPlugin::isIdentity(double time)
     _from3->getValueAtTime(time,p3.x, p3.y);
     _from4->getValueAtTime(time,p4.x, p4.y);
 
-    bool topLeftEnabled,topRightEnabled,btmLeftEnabled,btmRightEnabled;
-    _topLeftEnabled->getValue(topLeftEnabled);
-    _topRightEnabled->getValue(topRightEnabled);
-    _btmRightEnabled->getValue(btmRightEnabled);
-    _btmLeftEnabled->getValue(btmLeftEnabled);
 
-    if ((!topLeftEnabled || (p1.x == topLeft.x && p1.y == topLeft.y)) &&
-        (!topRightEnabled || (p2.x == topRight.x && p2.y == topRight.y)) &&
-        (!btmRightEnabled || (p3.x == btmRight.x && p3.y == btmRight.y)) &&
-        (!btmLeftEnabled || (p4.x == btmLeft.x && p4.y == btmLeft.y)) &&
+    if ((p1.x == topLeft.x && p1.y == topLeft.y) &&
+        (p2.x == topRight.x && p2.y == topRight.y) &&
+        (p3.x == btmRight.x && p3.y == btmRight.y) &&
+        (p4.x == btmLeft.x && p4.y == btmLeft.y) &&
         extraMat.isIdentity()) {
         return true;
     }

Misc/ofxsTransform3x3.cpp
@@ -172,7 +172,9 @@ Transform3x3Plugin::getRegionOfDefinition(const RegionOfDefinitionArguments &arg
     OfxRectD srcRoD = srcClip_->getRegionOfDefinition(args.time);
 
     ///if is identity return the input rod instead of transforming
-    if (isIdentity(args.time)) {
+    OFX::Clip* identityClip;
+    double identityTime;
+    if (isIdentity_internal(args.time,identityClip,identityTime)) {
         rod = srcRoD;
         return true;
     }
@@ -439,28 +441,33 @@ Transform3x3Plugin::render(const OFX::RenderArguments &args)
     }
 }
 
-// overridden is identity
-bool Transform3x3Plugin::isIdentity(const RenderArguments &args, Clip * &identityClip, double &identityTime)
+bool Transform3x3Plugin::isIdentity_internal(double time, OFX::Clip * &identityClip, double &identityTime)
 {
     // Transform3x3-GENERIC
-    if (isIdentity(args.time)) {
+    if (isIdentity(time)) {
         identityClip = srcClip_;
-        identityTime = args.time;
+        identityTime = time;
         return true;
     }
-
+    
     // GENERIC
     if (_masked) {
         double mix;
-        _mix->getValueAtTime(args.time, mix);
+        _mix->getValueAtTime(time, mix);
         if (mix == 0.) {
             identityClip = srcClip_;
-            identityTime = args.time;
+            identityTime = time;
             return true;
         }
     }
-
+    
     return false;
+}
+
+// overridden is identity
+bool Transform3x3Plugin::isIdentity(const RenderArguments &args, Clip * &identityClip, double &identityTime)
+{
+    return isIdentity_internal(args.time, identityClip, identityTime);
 }
 
 #ifdef OFX_EXTENSIONS_NUKE

Misc/ofxsTransform3x3.h
@@ -94,6 +94,8 @@ private:
     /* set up and run a processor */
     void setupAndProcess(Transform3x3ProcessorBase &, const OFX::RenderArguments &args);
 
+    bool isIdentity_internal(double time, OFX::Clip * &identityClip, double &identityTime);
+    
 private:
     // Transform3x3-GENERIC
     OFX::BooleanParam* _invert;

commit 310602de86ca0e6b53a6c01dd49a4fc884eabc78

CornerPin: fix bug introduced in 0826664938d0af9f211bec7a000359f6094f6e8a

CornerPin/CornerPin.cpp
@@ -414,11 +414,17 @@ bool CornerPinPlugin::isIdentity(double time)
     _from3->getValueAtTime(time,p3.x, p3.y);
     _from4->getValueAtTime(time,p4.x, p4.y);
 
-
-    if ((p1.x == topLeft.x && p1.y == topLeft.y) &&
-        (p2.x == topRight.x && p2.y == topRight.y) &&
-        (p3.x == btmRight.x && p3.y == btmRight.y) &&
-        (p4.x == btmLeft.x && p4.y == btmLeft.y) &&
+    bool topLeftEnabled, topRightEnabled, btmLeftEnabled, btmRightEnabled;
+    _topLeftEnabled->getValue(topLeftEnabled);
+    _topRightEnabled->getValue(topRightEnabled);
+    _btmRightEnabled->getValue(btmRightEnabled);
+    _btmLeftEnabled->getValue(btmLeftEnabled);
+    
+    // if topLeft is not enabled, q1 = p1 (see getInverseTransformCanonical()
+    if ((!topLeftEnabled || (p1.x == topLeft.x && p1.y == topLeft.y)) &&
+        (!topRightEnabled || (p2.x == topRight.x && p2.y == topRight.y)) &&
+        (!btmRightEnabled || (p3.x == btmRight.x && p3.y == btmRight.y)) &&
+        (!btmLeftEnabled || (p4.x == btmLeft.x && p4.y == btmLeft.y)) &&
         extraMat.isIdentity()) {
         return true;
     }