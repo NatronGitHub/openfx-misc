17802df043a7f7f7fab1637f7fc5778c0cb63781
Frédéric Devernay <frederic.devernay@m4x.org>
2014-10-22 18:52:00 +0200

    ColorLookup: sh*t happens

--- ColorLookup/ColorLookup.cpp
+++ ColorLookup/ColorLookup.cpp
@@ -344,8 +344,7 @@ private:
             double x = (value - _rangeMin) / (_rangeMax - _rangeMin);
             int i = (int)(x * nbValues);
             assert(0 <= i && i <= nbValues);
-            float alpha = x * nbValues - i;
-            assert(0 <= alpha && alpha < 1.);
+            float alpha = std::max(0.,std::min(x * nbValues - i, 1.));
             return _lookupTable[component][i] * (1.-alpha) + _lookupTable[component][i+1] * alpha;
         }
     }
