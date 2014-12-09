54ae50ccb2239494c7611223ca9e35701b029bfc
Frédéric Devernay <frederic.devernay@m4x.org>
2014-12-09 09:05:42 +0100

    Tracker: group setValues

--- TrackerPM/TrackerPM.cpp
+++ TrackerPM/TrackerPM.cpp
@@ -708,10 +708,12 @@ TrackerPMPlugin::setupAndProcess(TrackerPMProcessorBase &processor,
             newCenterPixelSub.x = refCenterPixelSub.x + bestMatch.x - refCenterI.x;
             newCenterPixelSub.y = refCenterPixelSub.y + bestMatch.y - refCenterI.y;
             OFX::MergeImages2D::toCanonicalSub(newCenterPixelSub, rsOne, par, &newCenter);
+            beginEdit();
             // create a keyframe at starting point
             _center->setValueAtTime(refTime, refCenter.x, refCenter.y);
             // create a keyframe at end point
             _center->setValueAtTime(otherTime, newCenter.x, newCenter.y);
+            endEdit();
         }
     }
 }
