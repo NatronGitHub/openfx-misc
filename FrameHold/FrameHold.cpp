7424b91dbd6d722fcbe1c78c9e655e70e8a59f2d
Frédéric Devernay <frederic.devernay@m4x.org>
2015-03-16 15:05:49 +0100

    FrameHost: fixlet

--- FrameHold/FrameHold.cpp
+++ FrameHold/FrameHold.cpp
@@ -131,7 +131,7 @@ public:
         _firstFrame = fetchIntParam(kParamFirstFrame);
         _increment = fetchIntParam(kParamIncrement);
         _sublabel = fetchStringParam(kNatronOfxParamStringSublabelName);
-        assert(_firstFrame && _increment);
+        assert(_firstFrame && _increment && _sublabel);
     }
 
 private:
