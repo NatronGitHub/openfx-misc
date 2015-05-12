956b77754ee37844e5379e15638b03da83a09ed9
Frédéric Devernay <frederic.devernay@m4x.org>
2015-05-12 17:01:52 +0200

    Merge: add reference to the Porter-Duff paper in the doc

--- Merge/Merge.cpp
+++ Merge/Merge.cpp
@@ -86,7 +86,11 @@
 
 #define kPluginName "MergeOFX"
 #define kPluginGrouping "Merge"
-#define kPluginDescription "Pixel-by-pixel merge operation between the two inputs."
+#define kPluginDescription \
+"Pixel-by-pixel merge operation between the two inputs.\n" \
+"A complete explanation of the different operators can be found in \Compositing Digital Images\", by T. Porter & T. Duff (Proc. SIGGRAPH 1984) <http://keithp.com/~keithp/porterduff/p253-porter.pdf>.\n" \
+"See also the course \"Digital Image Compositing\" by Marc Levoy <https://graphics.stanford.edu/courses/cs248-06/comp/comp.html>."
+
 #define kPluginIdentifier "net.sf.openfx.MergePlugin"
 #define kPluginVersionMajor 1 // Incrementing this number means that you have broken backwards compatibility of the plug-in.
 #define kPluginVersionMinor 0 // Increment this when you have fixed a bug or made it faster.
