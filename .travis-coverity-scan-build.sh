d0c35f9aeb242853a16db5883844c3c96f5295eb
Frédéric Devernay <frederic.devernay@m4x.org>
2015-12-05 17:57:42 +0100

    coverity fix: install tool in /opt (/tmp only is 1Gb, which is not enough)

--- .travis-coverity-scan-build.sh
+++ .travis-coverity-scan-build.sh
@@ -18,7 +18,7 @@ echo -e "\033[33;1mNote: COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN are 
 PLATFORM=`uname`
 TOOL_ARCHIVE=/tmp/cov-analysis-${PLATFORM}.tgz
 TOOL_URL=https://scan.coverity.com/download/${PLATFORM}
-TOOL_BASE=/tmp/coverity-scan-analysis
+TOOL_BASE=/opt/coverity-scan-analysis
 UPLOAD_URL="https://scan.coverity.com/builds"
 SCAN_URL="https://scan.coverity.com"
 
