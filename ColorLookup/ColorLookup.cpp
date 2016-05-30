e549ce51e0d1a5fe68de4f1d626ae22b02cbca16
Frédéric Devernay <frederic.devernay@m4x.org>
2016-05-30 14:50:20 +0200

    ColorLookup: add OVERRIDE FINAL to the background interact

--- ColorLookup/ColorLookup.cpp
+++ ColorLookup/ColorLookup.cpp
@@ -746,7 +746,7 @@ public:
     {
         _param = effect->fetchParametricParam(paramName);
     }
-    virtual bool draw(const OFX::DrawArgs &args)
+    virtual bool draw(const OFX::DrawArgs &args) OVERRIDE FINAL
     {
         glBegin (GL_POLYGON);
 
