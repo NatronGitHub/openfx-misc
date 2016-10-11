a66930aa5a48fd17015eee75860b345ab3dbb3f3
Frédéric Devernay <frederic.devernay@m4x.org>
2016-10-11 22:49:20 +0200

    EdgeExtend: doc typo

--- CImg/Blur/CImgBlur.cpp
+++ CImg/Blur/CImgBlur.cpp
@@ -178,7 +178,7 @@ OFXS_NAMESPACE_ANONYMOUS_ENTER
 #define kPluginNameEdgeExtend          "EdgeExtendCImg"
 #define kPluginDescriptionEdgeExtend \
 "Fill a matte (i.e. a non-opaque color image with an alpha channel) by extending the edges of the matte. This effect does nothing an an opaque image.\n" \
-"If the input matte comes from a keyer, the alpha channel of the matte should be first eroded by a small mount to remove pixels containing mixed foreground/background colors. If not, these mixed colors may be extended instead of the pure foreground colors.\n" \
+"If the input matte comes from a keyer, the alpha channel of the matte should be first eroded by a small amount to remove pixels containing mixed foreground/background colors. If not, these mixed colors may be extended instead of the pure foreground colors.\n" \
 "The filling process works by iteratively blurring the image, and merging the non-blurred image over the image to get to the next iteration. There are exactly 'Slices' such operations. The blur size at each iteration is linearly increasing.\n" \
 "'Size' is thus the total size of the edge extension, and 'Slices' is an indicator of the precision: the more slices there are, the sharper is the final image near the original edges.\n" \
 "Optionally, the image can be multiplied by the alpha channel on input (premultiplied), and divided by the alpha channel on output (unpremultiplied), so that if RGB contain an image and Alpha contains a mask, the output is an image where the RGB is smeared from the non-zero areas of the mask to the zero areas of the same mask.\n" \
