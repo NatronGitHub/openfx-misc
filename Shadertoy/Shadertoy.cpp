cef077aca45927ecd49f15c37c7913999fb47e44
Frédéric Devernay <frederic.devernay@m4x.org>
2017-01-08 16:51:39 +0100

    doc fixes

--- Shadertoy/Shadertoy.cpp
+++ Shadertoy/Shadertoy.cpp
@@ -671,7 +671,7 @@ using std::string;
 #define kParamEnableGPUHint \
     "Enable GPU-based OpenGL render.\n" \
     "If the checkbox is checked but is not enabled (i.e. it cannot be unchecked), GPU render can not be enabled or disabled from the plugin and is probably part of the host options.\n" \
-    "If the checkbox is not checked and is not enabled (i.e. it cannot be checked), GPU render is not available on this host.\n"
+    "If the checkbox is not checked and is not enabled (i.e. it cannot be checked), GPU render is not available on this host."
 #endif
 
 #ifdef HAVE_OSMESA
