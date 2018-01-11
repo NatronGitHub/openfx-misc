9ad3b43a346551f4e2dfd6546d6ee99a810bf006
Dr. Chat <arkolbed@gmail.com>
2018-01-10 18:10:24 -0600

    Windows.h -> windows.h

--- ContactSheet/ContactSheet.cpp
+++ ContactSheet/ContactSheet.cpp
@@ -31,7 +31,7 @@
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
-#include <Windows.h>
+#include <windows.h>
 #endif
 
 #include <GL/gl.h>
--- DenoiseSharpen/DenoiseWavelet.cpp
+++ DenoiseSharpen/DenoiseWavelet.cpp
@@ -27,7 +27,7 @@
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
-#include <Windows.h>
+#include <windows.h>
 #endif
 
 #include "ofxsMaskMix.h"
